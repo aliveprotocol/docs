1d1ab2f178542656d6edfe299ea66c1d48f2067b
techcoderx <engtianxi@outlook.com>
2021-02-02 15:09:06 +0800

    homepage translation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,72 +5,86 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import Translate, { translate } from '@docusaurus/Translate';
 
 const features = [
   {
-    title: 'Stream the way you want',
+    title: translate({
+      id: 'homepage.highlight.1.title',
+      description: 'Title for homepage highlight #1',
+      message: 'Stream the way you want'
+    }),
     imageUrl: 'img/undraw_choose.svg',
-    description: (
-      <>
-        Alive is built as a live streaming protocol that is actually decentralized which
-        any video DApp can take advantage of. Choose your favorite DApp and file sharing
-        protocol to stream to.
-      </>
-    )
+    description: translate({
+      id: 'homepage.highlight.1.desc',
+      description: 'Description for homepage highlight #1',
+      message: 'Alive is built as a live streaming protocol that is actually decentralized which any video DApp can take advantage of. Choose your favorite DApp and file sharing protocol to stream to.'
+    })
   },
   {
-    title: 'Communicate freely',
+    title: translate({
+      id: 'homepage.highlight.2.title',
+      description: 'Title for homepage highlight #2',
+      message: 'Communicate freely'
+    }),
     imageUrl: 'img/undraw_video_influencer.svg',
-    description: (
-      <>
-        Stream your content and interact with your viewers without worrying about
-        arbitrary censorship as Alive does not the authority to ban anyone on the
-        immutable blockchains.
-      </>
-    )
+    description: translate({
+      id: 'homepage.highlight.2.desc',
+      description: 'Description for homepage highlight #2',
+      message: 'Stream your content and interact with your viewers without worrying about arbitrary censorship as Alive does not the authority to ban anyone on the immutable blockchains.'
+    })
   },
   {
-    title: 'Highly scalable',
+    title: translate({
+      id: 'homepage.highlight.3.title',
+      description: 'Title for homepage highlight #3',
+      message: 'Highly scalable'
+    }),
     imageUrl: 'img/undraw_connected_world.svg',
-    description: (
-      <>
-        Alive is not limited to the scalability of a single blockchain as it is designed
-        to be built on multiple blockchains. Your video streams are spread out across
-        different IPFS/Skynet nodes around the world too.
-      </>
-    )
+    description: translate({
+      id: 'homepage.highlight.3.desc',
+      description: 'Description for homepage highlight #3',
+      message: 'Alive is not limited to the scalability of a single blockchain as it is designed to be built on multiple blockchains. Your video streams are spread out across different IPFS/Skynet nodes around the world too.'
+    })
   },
   {
-    title: 'Instant stream archive availability',
+    title: translate({
+      id: 'homepage.highlight.4.title',
+      description: 'Title for homepage highlight #4',
+      message: 'Instant stream archive availability'
+    }),
     imageUrl: 'img/undraw_video_streaming.svg',
-    description: (
-      <>
-        Just missed a stream from your favourite streamers? Stream archives of ended on-chain
-        Alive streams are made available instantly so that you can watch it immediately.
-      </>
-    )
+    description: translate({
+      id: 'homepage.highlight.4.desc',
+      description: 'Description for homepage highlight #4',
+      message: 'Just missed a stream from your favourite streamers? Stream archives of ended on-chain Alive streams are made available instantly so that you can watch it immediately.'
+    })
   },
   {
-    title: 'Moderate your own chat',
+    title: translate({
+      id: 'homepage.highlight.5.title',
+      description: 'Title for homepage highlight #5',
+      message: 'Moderate your own chat'
+    }),
     imageUrl: 'img/undraw_manage_chats.svg',
-    description: (
-      <>
-        The decentralization of Alive extends to live chat such that streamers and
-        streamer-approved moderators are the ones that have moderation powers to keep
-        the chat clean.
-      </>
-    )
+    description: translate({
+      id: 'homepage.highlight.5.desc',
+      description: 'Description for homepage highlight #5',
+      message: 'The decentralization of Alive extends to live chat such that streamers and streamer-approved moderators are the ones that have moderation powers to keep the chat clean.'
+    })
   },
   {
-    title: 'Developer friendly',
+    title: translate({
+      id: 'homepage.highlight.6.title',
+      description: 'Title for homepage highlight #6',
+      message: 'Developer friendly'
+    }),
     imageUrl: 'img/undraw_code_inspection.svg',
-    description: (
-      <>
-        Alive uses HLS to deliver streams, the industry leading communication protocol
-        used by many video players. All it takes to deliver Alive streams is just using
-        m3u8 playlists provided by Alive-enabled API nodes.
-      </>
-    )
+    description: translate({
+      id: 'homepage.highlight.6.desc',
+      description: 'Description for homepage highlight #6',
+      message: 'Alive uses HLS to deliver streams, the industry leading communication protocol used by many video players. All it takes to deliver Alive streams is just using m3u8 playlists provided by Alive-enabled API nodes.'
+    })
   }
 ]
 
@@ -106,7 +120,9 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
-              Get Started
+              <Translate id="homepage.button.getstarted" description="Get started button text on homepage">
+                Get Started
+              </Translate>
             </Link>
           </div>
         </div>
